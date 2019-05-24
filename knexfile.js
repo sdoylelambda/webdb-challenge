e4b2c1c6fb55c35c1cff9445a03fdef07bdf53c9
Sean Doyle <sdoylelambda@gmail.com>
2019-05-24 12:01:10 -0400

    knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      // check db name  !!!!!!
+      filename: './data/lambda.db3', // the folder will be created when we run the migrations
+    },
+    // STEP 1
+    migrations: {
+      directory: './data/migrations'
+    },
+    // STEP 2
+    seeds: {
+      directory: './data/seeds'
+    },
+    // STEP 3 
+    // in terminal run npx knex migrate:make bootstrap
+    // it should auto put in data folder, if not move if there when done
+    
+    // STEP 8
+
+
+
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run('PRAGMA foreign_keys = ON', done)
+    //   }
+    // }
+  },
+};
